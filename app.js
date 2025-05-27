ba7f1a518b20556469746b9ac651d043bf7b7074
menso431 <menson.sundash@gmail.com>
2025-05-27 21:11:18 +1000

    script to add and display movies

---
+++ app.js
@@ -0,0 +1,50 @@
+class Movie {
+    constructor(title, genre, releaseYear) {
+        this.title = title;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+    }
+    
+
+    getInfo() {
+        return `${this.title} is a ${this.genre} movie released in ${this.releaseYear}`;
+    }
+}
+
+let movies = [];
+
+function addMovie() {
+    let title = document.getElementById('movieTitle').value;
+    let genre = document.getElementById('movieGenre').value;
+    let year = parseInt(document.getElementById('releaseYear').value);
+
+    //checking for empty fields
+    if(!title || !genre || isNaN(year)) {
+        alert("Please fill in all fields correctly.");
+        return;
+    }
+
+    //creating object and add new Movies into array
+    const newMovie = new Movie(title, genre, year);
+    movies.push(newMovie);
+
+    displayMovies();
+
+    //clear form inputs
+    title = '';
+    genre = '';
+    year = '';
+}
+
+function displayMovies(){
+    const list = document.getElementById('movieList');
+    
+    list.innerHTML = ''; //Clear old lines
+
+    for(let movie of movies) {
+        const li = document.createElement('li');
+
+        li.textContent = movie.getInfo();
+        list.appendChild(li);
+    }
+}
